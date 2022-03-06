3b9c601260d304c9bc71bc02b8d4011f9fd3899d
sargis <abovyan.sargis90@gmail.com>
2022-03-06 13:33:07 +0400

    useReducer add reducer with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,81 @@
 import { useState, useRef, useEffect, useReducer } from "react";
 import "./App.css";
 
-const initialState = { count: 0 };
+const requestStatus = {
+  initial: "initial",
+  loading: "loading",
+  error: "error",
+  fulfilled: "fulfilled",
+};
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    case "reset":
+const initialState = {
+  status: requestStatus.initial,
+  data: null,
+  error: null,
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case requestStatus.initial:
       return initialState;
-    case "multiply":
-      return { count: state.count * action.payload.number };
+
+    case requestStatus.loading:
+      return { ...state, status: requestStatus.loading };
+
+    case requestStatus.fulfilled:
+      return { ...state, data: payload.data, status: requestStatus.fulfilled };
+
+    case requestStatus.error:
+      return { ...state, status: requestStatus.error, error: payload.error };
+
     default:
       throw new Error();
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <>
-      Count: {state.count}
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "reset" })}>null</button>
-      <button
-        onClick={() => dispatch({ type: "multiply", payload: { number: 2 } })}
-      >
-        x 2
-      </button>
-      <button
-        onClick={() => dispatch({ type: "multiply", payload: { number: 3 } })}
-      >
-        x 3
-      </button>
-    </>
+  const [{ status, data, error }, dispatch] = useReducer(
+    reducer,
+    initialState,
+    (state) => {
+      return { ...state, status: requestStatus.loading };
+    }
   );
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(
+          "https://restcountries.com/v3.1/name/aruba?fullText=true"
+        );
+
+        const json = await res.json();
+
+        if (!res.ok) {
+          throw new Error(json.message);
+        }
+
+        dispatch({ type: requestStatus.fulfilled, payload: { data: json } });
+      } catch (err) {
+        dispatch({
+          type: requestStatus.error,
+          payload: { error: { message: err.message } },
+        });
+      }
+    })();
+  }, []);
+
+  if (status === requestStatus.loading) {
+    return <div>loading</div>;
+  }
+
+  if (status === requestStatus.error && error?.message) {
+    return <div>{error.message}</div>;
+  }
+
+  return status === requestStatus.fulfilled && data?.length ? (
+    <div>{JSON.stringify(data)}</div>
+  ) : null;
 }
 
 export default App;
