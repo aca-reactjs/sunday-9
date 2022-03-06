c4b72deb2e390b17d5c36118571176627d021382
sargis <abovyan.sargis90@gmail.com>
2022-03-06 12:32:44 +0400

    carousel implement navigation

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,50 @@ const buttonType = {
 };
 
 function App() {
-  const handleButtonClick = (type) => () => {};
+  const [activeElementPosition, setActiveElementPosition] = useState(0);
+  const nextButtonRef = useRef();
+  const prevButtonRef = useRef();
+
+  // const [disabledButton, setDisabledButton] = useState(buttonType.prev);
+
+  const handleButtonClick = (type) => () => {
+    if (type === buttonType.next) {
+      if (activeElementPosition <= 1) {
+        setActiveElementPosition((prev) => prev + 1);
+        // setDisabledButton(false);
+        prevButtonRef.current.disabled = false;
+      } else {
+        // setDisabledButton(buttonType.next);
+        nextButtonRef.current.disabled = true;
+      }
+
+      return;
+    }
+
+    if (activeElementPosition >= 1) {
+      setActiveElementPosition((prev) => prev - 1);
+      nextButtonRef.current.disabled = false;
+      // setDisabledButton(false);
+    } else {
+      prevButtonRef.current.disabled = true;
+      // setDisabledButton(buttonType.prev);
+    }
+  };
+
+  useEffect(() => {
+    prevButtonRef.current.disabled = true;
+  }, []);
 
   return (
     <div>
       <div className="screen">
-        <div className="slider-wrapper">
+        <div
+          className="slider-wrapper"
+          style={{
+            transform: `translateX(-${activeElementPosition * 200}px)`,
+            transition: "transform 200ms linear",
+          }}
+        >
           {array.map((el) => (
             <div className="slider-item" key={el}>
               {el}
@@ -23,14 +61,18 @@ function App() {
         </div>
       </div>
 
-      <div className="button-wrapper" style={{}}>
+      <div className="button-wrapper">
         <button
-          onClick={handleButtonClick(buttonType.next)}
+          ref={prevButtonRef}
+          // disabled={disabledButton === buttonType.prev}
+          onClick={handleButtonClick(buttonType.prev)}
           className="slider-control"
         >
           prev
         </button>
         <button
+          ref={nextButtonRef}
+          // disabled={disabledButton === buttonType.next}
           onClick={handleButtonClick(buttonType.next)}
           className="slider-control"
         >
